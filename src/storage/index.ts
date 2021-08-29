53a04c261f5cab48851800f144278e3df56b606f
ImDanTheDev <dmott248@gmail.com>
2021-08-29 00:26:04 -0400

    Implemented storage api from @omnifire

---
+++ src/storage/index.ts
@@ -0,0 +1,7 @@
+import FirebaseStorageProvider from "./FirebaseStorageProvider";
+import IStorageProvider from "./IStorageProvider";
+
+export {
+    FirebaseStorageProvider,
+    IStorageProvider
+}
